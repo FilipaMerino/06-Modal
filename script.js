35628eaf5ae9a1c67d91a0498eeca42c73db0750
FilipaMerino <filipaam@outlook.com>
2024-05-07 18:21:17 +0100

    open modal when click

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay")
-const btnCloseModal = document.querySelector(".close-modal")
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btns = document.querySelectorAll('.show-modal');
 
-const btns = document.querySelectorAll(".show-modal");
 
-let btnsOpenModal = []
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    modal.classList.remove("hidden");
+  });
 
-for (let i = 0; i < btns.length; i++){
-  btnsOpenModal.push(btns[i].textContent);
 }
