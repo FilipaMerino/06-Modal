a570d301f12497300c7db36afe9182908f707a48
FilipaMerino <filipaam@outlook.com>
2024-05-07 18:14:03 +0100

    Store elements into variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay")
+const btnCloseModal = document.querySelector(".close-modal")
+
+const btns = document.querySelectorAll(".show-modal");
+
+let btnsOpenModal = []
+
+for (let i = 0; i < btns.length; i++){
+  btnsOpenModal.push(btns[i].textContent);
+}
