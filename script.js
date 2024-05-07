333108678d6d8e92c251bfe6f60d7000ef85f933
FilipaMerino <filipaam@outlook.com>
2024-05-07 18:26:51 +0100

    Remove and add overlay

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ const btns = document.querySelectorAll('.show-modal');
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     modal.classList.remove("hidden");
+    overlay.classList.remove("hidden")
   });
 }
 
 btnCloseModal.addEventListener("click", function(){
   modal.classList.add("hidden")
+  overlay.classList.add("hidden")
 })
