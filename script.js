3d2e98637a036f6a561476033d33af6bacb72023
FilipaMerino <filipaam@outlook.com>
2024-05-07 18:32:46 +0100

    Formatting

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btns = document.querySelectorAll('.show-modal');
 
-
 for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden")
+  btns[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 }
 
-btnCloseModal.addEventListener("click", function(){
-  modal.classList.add("hidden")
-  overlay.classList.add("hidden")
-})
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
